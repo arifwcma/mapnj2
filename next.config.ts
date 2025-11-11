66dd262676bd8d86ce40986101a2bf154ef743fd
arifwcma <arif.rahman@wcma.vic.gov.au>
2025-11-12 09:51:41 +1100

    done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.0/24'],
+  allowedDevOrigins: ['192.168.1.65', '192.168.1.0/24'],
 };
 
 export default nextConfig;
